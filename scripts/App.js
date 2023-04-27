5f0a0974f13ae2f7cfb86969ff9a1c44717dac01
OhHyeonJu0415 <guswn2209@gmail.com>
2023-04-27 19:05:06 +0900

    feat : 헤더 이동 구현

---
+++ scripts/App.js
@@ -0,0 +1,24 @@
+class App {
+  constructor($container) {
+    this.$container = $container;
+    this.render();
+  }
+
+  navEvent() {
+    const header = document.getElementsByClassName("move");
+
+    const movement = [0, 1100, 2620, 200000];
+
+    for (let i = 0; i < 4; i++) {
+      header[i].addEventListener("click", () => {
+        window.scrollTo({ left: 0, top: movement[i], behavior: "smooth" });
+      });
+    }
+  }
+
+  render() {
+    this.navEvent();
+  }
+}
+
+export default App;
