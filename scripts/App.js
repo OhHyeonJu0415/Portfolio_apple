eae5cc9c46f73cc98d49dca27511bd8d9b580e6c
OhHyeonJu0415 <guswn2209@gmail.com>
2023-04-28 11:42:21 +0900

    refactor : 컴포넌트 분리

--- scripts/App.js
+++ scripts/App.js
@@ -18,53 +18,8 @@ class App {
     }
   }
 
-  projectEvent() {
-    const btn = document.getElementsByClassName("arrow");
-    const modalContainer = document.getElementsByClassName("modalContainer");
-    const modalScrim = document.getElementsByClassName("modalScrim");
-    const modalPanel = document.getElementsByClassName("modalPanel");
-    const panelShow = document.getElementsByClassName("panelShow");
-    const main = document.getElementsByClassName("main");
-    const body = document.getElementById("container");
-
-    for (let i = 0; i < 5; i++) {
-      btn[i].addEventListener("click", () => {
-        modalContainer[i].classList.add("visib");
-        modalScrim[i].classList.add("visib");
-        modalPanel[i].classList.add("modalPanelShow");
-        panelShow[i].classList.add("moveRight");
-        main[0].classList.add("moveLeft");
-        body.classList.add("scroll");
-      });
-    }
-  }
-
-  closeEvent() {
-    const modalClose = document.getElementsByClassName("modalClose");
-    const modalContainer = document.getElementsByClassName("modalContainer");
-    const modalScrim = document.getElementsByClassName("modalScrim");
-    const modalPanel = document.getElementsByClassName("modalPanel");
-    const panelShow = document.getElementsByClassName("panelShow");
-    const main = document.getElementsByClassName("main");
-    const body = document.getElementById("container");
-
-    for (let i = 0; i < 5; i++) {
-      modalClose[i].addEventListener("click", () => {
-        setTimeout(() => modalContainer[i].classList.remove("visib"), 1000);
-        setTimeout(() => modalScrim[i].classList.remove("visib"), 1000);
-        setTimeout(() => body.classList.remove("scroll"), 1000);
-
-        modalPanel[i].classList.remove("modalPanelShow");
-        panelShow[i].classList.remove("moveRight");
-        main[0].classList.remove("moveLeft");
-      });
-    }
-  }
-
   render() {
     this.navEvent();
-    // this.projectEvent();
-    // this.closeEvent();
     new Modal(this.$container);
   }
 }
---
+++ scripts/Modal.js
@@ -0,0 +1,82 @@
+class Modal {
+  constructor($container) {
+    this.$container = $container;
+    this.render();
+  }
+
+  projectEvent(
+    btn,
+    main,
+    body,
+    modalContainer,
+    modalScrim,
+    modalPanel,
+    panelShow
+  ) {
+    for (let i = 0; i < 5; i++) {
+      btn[i].addEventListener("click", () => {
+        main.classList.add("moveLeft");
+        body.classList.add("scroll");
+        modalContainer[i].classList.add("visib");
+        modalScrim[i].classList.add("visib");
+        modalPanel[i].classList.add("modalPanelShow");
+        panelShow[i].classList.add("moveRight");
+      });
+    }
+  }
+
+  closeEvent(
+    modalClose,
+    main,
+    modalContainer,
+    modalScrim,
+    modalPanel,
+    panelShow,
+    body
+  ) {
+    for (let i = 0; i < 5; i++) {
+      modalClose[i].addEventListener("click", () => {
+        setTimeout(() => modalContainer[i].classList.remove("visib"), 1000);
+        setTimeout(() => modalScrim[i].classList.remove("visib"), 1000);
+        setTimeout(() => body.classList.remove("scroll"), 1000);
+
+        modalPanel[i].classList.remove("modalPanelShow");
+        panelShow[i].classList.remove("moveRight");
+        main.classList.remove("moveLeft");
+      });
+    }
+  }
+
+  render() {
+    const btn = document.getElementsByClassName("arrow");
+    const modalContainer = document.getElementsByClassName("modalContainer");
+    const modalScrim = document.getElementsByClassName("modalScrim");
+    const modalPanel = document.getElementsByClassName("modalPanel");
+    const panelShow = document.getElementsByClassName("panelShow");
+    const main = document.getElementsByClassName("main");
+    const body = document.getElementById("container");
+    const modalClose = document.getElementsByClassName("modalClose");
+
+    this.projectEvent(
+      btn,
+      main[0],
+      body,
+      modalContainer,
+      modalScrim,
+      modalPanel,
+      panelShow
+    );
+
+    this.closeEvent(
+      modalClose,
+      main[0],
+      modalContainer,
+      modalScrim,
+      modalPanel,
+      panelShow,
+      body
+    );
+  }
+}
+
+export default Modal;
