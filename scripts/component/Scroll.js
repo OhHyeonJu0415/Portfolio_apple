f9c8ffd5048f4e03dd9dd58fbbe3c93832805647
OhHyeonJu0415 <guswn2209@gmail.com>
2023-04-29 14:45:15 +0900

    feat : 프로젝트 타이틀 애니메이션 구현

--- scripts/component/Scroll.js
+++ scripts/component/Scroll.js
@@ -7,21 +7,41 @@ class Scroll {
     const yoohooObj = document.getElementsByClassName("yoohoo")[0];
     const covers = document.getElementsByClassName("projectCover");
 
+    const titles = document.getElementsByClassName("coverTitle");
+
+    const travleTitle = document.getElementById("travelTitle");
+
     window.addEventListener("scroll", () => {
       let H = window.scrollY + window.innerHeight; //현재 스크롤 하단 y좌표
 
-      if (H >= travelObj.offsetTop + 160) {
+      if (H >= travelObj.offsetTop + 120) {
+        //travel 프로젝트 타이틀 애니메이션
+        titles[0].classList.add("opacityAni");
+      }
+
+      if (H >= travelObj.offsetTop + 180) {
         //travel, fm 프로젝트 커버 애니메이션
         covers[0].classList.add("travelAni");
         covers[1].classList.add("fmAni");
       }
 
-      if (H >= travelObj.offsetTop + 470) {
-        //toy 프로젝트 커버 애니메이션
-        covers[2].classList.add("toyAni");
+      if (H >= travelObj.offsetTop + 320) {
+        //fm 프로젝트 타이틀 애니메이션
+        titles[1].classList.add("opacityAni");
       }
 
-      if (H >= yoohooObj.offsetTop + 140) {
+      if (H >= travelObj.offsetTop + 470) {
+        //toy 프로젝트 커버,타이틀 애니메이션
+        covers[2].classList.add("toyAni");
+        titles[2].classList.add("opacityAni");
+      }
+
+      if (H >= yoohooObj.offsetTop + 230) {
+        //yoohoo 프로젝트 타이틀 애니메이션
+        titles[7].classList.add("opacityAni");
+      }
+
+      if (H >= yoohooObj.offsetTop + 320) {
         //yoohoo 프로젝트 커버 애니메이션
         covers[3].classList.add("yoohooAni");
       }
