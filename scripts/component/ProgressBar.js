16c58cb683e136d006b5fde67643710f4486731c
OhHyeonJu0415 <guswn2209@gmail.com>
2023-05-05 16:55:56 +0900

    feat : 프로젝트 상세보기 마우스 휠 이벤트 추가

--- scripts/component/ProgressBar.js
+++ scripts/component/ProgressBar.js
@@ -5,25 +5,50 @@ class ProgressBar {
     this.render();
   }
 
-  setProgress() {
-    let wrap = this.$container;
+  // setProgress() {
+  //   let wrap = this.$container;
+  //   let bar = this.$bar;
+  //   wrap.addEventListener("scroll", function () {
+  //     let scrollLeft = wrap.scrollLeft;
+  //     let scrollWidth = wrap.scrollWidth - wrap.clientWidth;
+  //     let percentage = scrollLeft / scrollWidth;
+  //     bar.style.background = `linear-gradient(
+  //       90deg,
+  //       white,
+  //       white calc(${percentage} * 100%),
+  //       rgba(255, 255, 255, 0.4) calc(${percentage} * 100%),
+  //       rgba(255, 255, 255, 0.4)
+  //     )`;
+  //   });
+  // }
+
+  setMouseWheel() {
     let bar = this.$bar;
-    wrap.addEventListener("scroll", function () {
-      let scrollLeft = wrap.scrollLeft;
-      let scrollWidth = wrap.scrollWidth - wrap.clientWidth;
-      let percentage = scrollLeft / scrollWidth;
+    let wrap = this.$container;
+    let percentage = wrap.scrollLeft;
+    wrap.addEventListener("wheel", function (event) {
+      event.preventDefault();
+      if (event.deltaY < 0) {
+        //프로그레스바 왼쪽으로 이동
+        if (percentage > 0) percentage -= 0.1;
+      } else {
+        //프로그레스바 오른쪽으로 이동
+        if (percentage < 1) percentage += 0.1;
+      }
       bar.style.background = `linear-gradient(
-        90deg,
-        white,
-        white calc(${percentage} * 100%),
-        rgba(255, 255, 255, 0.4) calc(${percentage} * 100%),
-        rgba(255, 255, 255, 0.4)
-      )`;
+          90deg,
+          white,
+          white calc(${percentage} * 100%),
+          rgba(255, 255, 255, 0.4) calc(${percentage} * 100%),
+          rgba(255, 255, 255, 0.4)
+        )`;
+      wrap.scrollLeft = (wrap.scrollWidth - wrap.clientWidth) * percentage;
     });
   }
 
   render() {
-    this.setProgress();
+    this.setMouseWheel();
+    // this.setProgress();
   }
 }
 
